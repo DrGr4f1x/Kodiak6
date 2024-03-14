f9747e7926082b38ad5bf3672cf9918d19356aaa
Dave Elder <elddm1@gmail.com>
2024-03-13 20:57:19 -0700

    Starting to stub out DeviceManager

---
+++ Engine/Graphics/DX12/DeviceManager12.h
@@ -0,0 +1,24 @@
+//
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// Author:  David Elder
+//
+
+#pragma once
+
+#include "Graphics\DeviceManager.h"
+
+namespace Kodiak::DX12
+{
+
+class DeviceManager12 : public DeviceManager
+{
+public:
+
+};
+
+} // namespace Kodiak::DX12
